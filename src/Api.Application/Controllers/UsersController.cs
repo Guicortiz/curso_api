63d87ce9f45f1622432ea78f00112c51cf329660
Guilherme Chenci Ortiz <guilhermechenciortiz@outlook.com>
2021-12-20 21:57:00 -0300

    Terminada camada de autenticacao
    Criado dto para Login, configurado requisisoes do token jwt, gerar token jwt e atualizar swagger.

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Threading.Tasks;
 using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Application.Controllers
@@ -16,6 +17,8 @@ namespace Api.Application.Controllers
         {
             _service = service;
         }
+
+        [Authorize("Bearer")]
         [HttpGet]
         public async Task<ActionResult> GetAll()
         {
@@ -34,6 +37,7 @@ namespace Api.Application.Controllers
             }
         }
 
+        [Authorize("Bearer")]
         [HttpGet]
         [Route("{id}", Name = "GetWithId")]
         public async Task<ActionResult> Get(Guid id)
@@ -53,6 +57,7 @@ namespace Api.Application.Controllers
             }
         }
 
+        [Authorize("Bearer")]
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] UserEntity user)
         {
@@ -79,6 +84,7 @@ namespace Api.Application.Controllers
             }
         }
 
+        [Authorize("Bearer")]
         [HttpPut]
         public async Task<ActionResult> Put([FromBody] UserEntity user)
         {
@@ -106,6 +112,7 @@ namespace Api.Application.Controllers
 
         }
 
+        [Authorize("Bearer")]
         [HttpDelete]
         [Route("{id}", Name = "DeleteWithId")]
         public async Task<ActionResult> Delete(Guid id)
--- src/Api.Service/Services/LoginService.cs
+++ src/Api.Service/Services/LoginService.cs
@@ -1,30 +1,107 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
+using Api.Domain.Dtos;
 using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
 using Api.Domain.Repository;
+using Api.Domain.Security;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Service.Services
 {
     public class LoginService : ILoginService
     {
         private IUserRepository _repository;
+        private SigningConfigurations _signingConfigurations;
+        private TokenConfigurations _tokenConfigurations;
 
-        public LoginService(IUserRepository repository)
+        private IConfiguration _configuration { get; }
+
+        public LoginService(IUserRepository repository,
+                            SigningConfigurations signingConfigurations,
+                            TokenConfigurations tokenConfigurations,
+                            IConfiguration configuration)
         {
             _repository = repository;
+            _signingConfigurations = signingConfigurations;
+            _tokenConfigurations = tokenConfigurations;
+            _configuration = configuration;
         }
 
-        public async Task<object> FindByLogin(UserEntity user)
+        public async Task<object> FindByLogin(LoginDto user)
         {
             var baseUser = new UserEntity();
             if (user != null && !string.IsNullOrWhiteSpace(user.Email))
             {
-                return await _repository.FindByLogin(user.Email);
+                baseUser = await _repository.FindByLogin(user.Email);
+                if (baseUser == null)
+                {
+                    return new
+                    {
+                        authenticated = false,
+                        message = "Falha ao autenticar"
+                    };
+                }
+                else
+                {
+                    var identy = new ClaimsIdentity(
+                        new GenericIdentity(baseUser.Email),
+                        new[]{
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                            new Claim(JwtRegisteredClaimNames.UniqueName, user.Email),
+                        }
+                    );
+
+                    DateTime createDate = DateTime.Now;
+                    DateTime expirationDate = createDate + TimeSpan.FromSeconds(_tokenConfigurations.Seconds);
+
+                    var handler = new JwtSecurityTokenHandler();
+                    string token = CreateToken(identy, createDate, expirationDate, handler);
+
+                    return SuccessObject(createDate, expirationDate, token, user);
+                }
             }
             else
             {
-                return null;
+                return new
+                {
+                    authenticated = false,
+                    message = "Falha ao autenticar"
+                };
             }
+        }
+
+        private string CreateToken(ClaimsIdentity identy, DateTime createDate, DateTime expirationDate, JwtSecurityTokenHandler handler)
+        {
+            var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _tokenConfigurations.Issuer,
+                Audience = _tokenConfigurations.Audience,
+                SigningCredentials = _signingConfigurations.SigningCredentials,
+                Subject = identy,
+                NotBefore = createDate,
+                Expires = expirationDate,
+            });
+
+            var token = handler.WriteToken(securityToken);
+            return token;
+        }
+
+        private object SuccessObject(DateTime createDate, DateTime expirationDate, string token, LoginDto user)
+        {
+            return new
+            {
+                authenticated = true,
+                created = createDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                expiration = expirationDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                acessToken = token,
+                userName = user.Email,
+                message = "Usuario Logado com sucesso"
+            };
         }
     }
 }
