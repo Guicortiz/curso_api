e475e7491916f0cbf4cb3d36b45e31e1559dfa0a
Guilherme Chenci Ortiz <guilhermechenciortiz@outlook.com>
2021-12-22 13:08:49 -0300

    Criado UserDtoCreate e UserDtoUpdate para as operações na api.

--- src/Api.Domain/Dtos/User/UserDto.cs
+++ src/Api.Domain/Dtos/User/UserDto.cs
@@ -1,16 +1,13 @@
-using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace Api.Domain.Dtos.User
 {
     public class UserDto
     {
-        [Required(ErrorMessage = "O campo Nome é obrigatorio")]
-        [StringLength(60, ErrorMessage = "Nome deve ter no maximo {1} caracteres.")]
+        public Guid Id { get; set; }
         public string Name { get; set; }
-
-        [Required(ErrorMessage = "O campo Nome é obrigatorio")]
-        [EmailAddress(ErrorMessage = "Email em formato invalido")]
-        [StringLength(100, ErrorMessage = "Nome deve ter no maximo {1} caracteres.")]
         public string Email { get; set; }
+        public DateTime CreateAt { get; set; }
+
     }
 }
---
+++ src/Api.Domain/Dtos/User/UserDtoCreate.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Dtos.User
+{
+    public class UserDtoCreate
+    {
+        [Required(ErrorMessage = "O campo Nome é obrigatorio")]
+        [StringLength(60, ErrorMessage = "Nome deve ter no maximo {1} caracteres.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatorio")]
+        [EmailAddress(ErrorMessage = "Email em formato invalido")]
+        [StringLength(100, ErrorMessage = "Nome deve ter no maximo {1} caracteres.")]
+        public string Email { get; set; }
+    }
+}
